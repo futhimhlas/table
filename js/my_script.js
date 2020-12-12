094dfefce9469f80f9760fa541eca434caafdf5e
futhimhlas <futhimhl2@gmail.com>
2020-12-12 15:09:52 +0000

    intial commit

---
+++ js/my_script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    //Anything inside the curly brackets after this function will only run after the entire dom is loaded
+    //$("element").addClass("ClassNameWithNoDot") use this to style rather than .css as this method doesn't use inline styling
+    //$("element").removeClass("ClassNameWithoutDot") removes a class from specified element
+    $("h2").css("text-decoration")
+});
